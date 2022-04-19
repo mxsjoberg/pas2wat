41edf855ba8912d319fc9992ca0931022bfaf735
Michael Sjöberg <random@michaelsjoberg.com>
2022-04-19 23:29:09 +0200

    Update token.rs

--- src/token.rs
+++ src/token.rs
@@ -2,60 +2,63 @@ use crate::r#type::Type;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Token {
-    // types
-    TYPE_SPEC(Type),
-    // numbers
-    INTEGER(i32),
-    REAL(f64),
-    RANGE(i32, i32),
-    // booleans
-    TRUE,
-    FALSE,
-    // operators
-    PLUS,
-    MINUS,
-    MULTIPLY,
-    DIVIDE,
-    EQUAL,
-    NOT_EQUAL,
-    GREATER_THAN,
-    GREATER_EQUAL,
-    LESS_THAN,
-    LESS_EQUAL,
-    // expressions
-    LPAR,
-    RPAR,
-    LBRA,
-    RBRA,
-    // variables
-    ID(String),
-    ASSIGN,
-    // program
-    BLOCK,
-    SEMICOLON,
-    COLON,
-    COMMA,
-    DOT,
-    // keywords
-    PROGRAM,
-    //TYPE,
-    VAR,
-    PACKED,
-    ARRAY,
-    OF,
-    INTEGER_DIV, // DIV
-    INTEGER_MOD, // MOD
-    BEGIN,
-    END,
-    EMPTY,
-    WHILE,
-    DO,
-    //BREAK,
-    IF,
-    THEN,
-    ELSE,
-    // functions
-    WRITELN,
-    // end of file
-    EOF
+  // beginning of file
+  BOF,
+  // types
+  TYPE_SPEC(Type),
+  // numbers
+  INTEGER(i32),
+  REAL(f64),
+  RANGE(i32, i32),
+  // booleans
+  TRUE,
+  FALSE,
+  // operators
+  PLUS,
+  MINUS,
+  MULTIPLY,
+  DIVIDE,
+  EQUAL,
+  NOT_EQUAL,
+  GREATER_THAN,
+  GREATER_EQUAL,
+  LESS_THAN,
+  LESS_EQUAL,
+  // expressions
+  LPAR,
+  RPAR,
+  LBRA,
+  RBRA,
+  // variables
+  ID(String),
+  ASSIGN,
+  // program
+  BLOCK,
+  SEMICOLON,
+  COLON,
+  COMMA,
+  DOT,
+  // keywords
+  PROGRAM,
+  //TYPE,
+  VAR,
+  PACKED,
+  ARRAY,
+  OF,
+  INTEGER_DIV, // DIV
+  INTEGER_MOD, // MOD
+  BEGIN,
+  END,
+  EMPTY,
+  WHILE,
+  DO,
+  ODD,
+  //BREAK,
+  IF,
+  THEN,
+  ELSE,
+  // functions
+  WRITELN,
+  // end of file
+  EOF
 }
