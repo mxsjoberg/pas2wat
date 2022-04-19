cf2e362b322b1da34889245c57aae709e57b7776
Michael Sjöberg <random@michaelsjoberg.com>
2022-04-19 23:28:55 +0200

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,11 @@
 // debug
 pub static DEBUG                        : bool = true;
-pub static DEBUG_WITH_INPUT             : bool = false;
-pub static DEBUG_SHOW_CHAR              : bool = true;
-pub static DEBUG_SHOW_TOKEN             : bool = true;
+pub static DEBUG_WITH_INPUT             : bool = true;
+pub static DEBUG_SHOW_CHAR              : bool = false;
+pub static DEBUG_SHOW_TOKEN             : bool = false;
 pub static DEBUG_SHOW_TREE              : bool = false;
 pub static DEBUG_SHOW_SYMBOL_TABLE      : bool = false;
 pub static DEBUG_SHOW_ASSIGNMENT_TABLE  : bool = false;
+pub static DEBUG_SHOW_RESULT            : bool = true;
 // output
 pub static OUTPUT_VERBOSE               : bool = true;
