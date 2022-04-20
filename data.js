cb0ca909ec73d8889167323cf6879b064cedc266
Michael Sjöberg <random@michaelsjoberg.com>
2022-04-20 22:22:42 +0200

    added data from memory example

--- data.js
+++ data.js
@@ -1,20 +1,14 @@
-var memory = new WebAssembly.Memory({ initial : 1 });
-
 function consoleLogString(offset, length) {
   var bytes = new Uint8Array(memory.buffer, offset, length);
   var string = new TextDecoder('utf8').decode(bytes);
   console.log(string);
 }
 
-var importObject = {
-  console: {
-    log: consoleLogString
-  },
-  js: {
-    mem: memory
-  }
-};
+function consoleLog(value) {
+  console.log(value);
+}
 
-const wasmInstance = new WebAssembly.Instance(wasmModule, importObject);
-const { main } = wasmInstance.exports;
-main();
+var memory = new WebAssembly.Memory({ initial: 1 });
+const wasmInstance = new WebAssembly.Instance(wasmModule, {console: { log: consoleLog, logString: consoleLogString }, js: { mem: memory } });
+const { test } = wasmInstance.exports;
+test();
--- data.wat
+++ data.wat
@@ -1,13 +1,13 @@
 (module
-  (import "console" "log" (func $log (param i32 i32)))
+  (import "console" "log" (func $log (param f64)))
+  (import "console" "logString" (func $logString (param i32 i32)))
   (import "js" "mem" (memory 1))
-  (data (i32.const 0) "hello wasm")
-  (func (export "main")
-  	;; pass offset 0 to log
-    i32.const 0
-    ;; pass length 10 to log
-    i32.const 10
-    ;; call log
+  (data (i32.const 0) "Hi")
+  (func (export "test")
+    i32.const 0  ;; pass offset 0 to log
+    i32.const 2  ;; pass length 2 to log
+    call $logString
+    f64.const 1
     call $log
   )
 )
