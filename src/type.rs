113a96cd26717458217720cd70709828734528d6
Michael Sjöberg <random@michaelsjoberg.com>
2022-04-20 22:22:44 +0200

    Update type.rs

--- src/type.rs
+++ src/type.rs
@@ -1,3 +1,21 @@
+/*
+
+  Types
+
+  INTEGER     : INTEGER(_)
+  LONGINT     : INTEGER(_)
+  REAL        : REAL(_)
+
+  TODO
+
+  SMALLINT    : INTEGER(_)
+  BOOLEAN     : INTEGER(1) | INTEGER(0)
+  STRING      : 
+  CHAR        :
+  BYTE        :
+
+*/
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum Type {
   INTEGER,
