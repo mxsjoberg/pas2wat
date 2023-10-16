4673a55b25e409ac9cbca0069e1cfb22e8773228
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-16 15:30:44 +0200

    Update ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -1,19 +1,27 @@
 // https://codebeautify.org/jsviewer
 
+use std::fmt;
+
 use crate::token::{ Token };
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct AST {
   pub token: Token,
   pub children: Vec<AST>,
 }
 
 impl AST {
-  // new : AST
+  // new() -> AST
   pub fn new(token: Token, children: Vec<AST>) -> AST {
     AST {
       token: token,
       children: children
     }
   }
+}
+
+impl std::fmt::Display for AST {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
