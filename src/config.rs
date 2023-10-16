796ef68375ef232daea585c1e80080046848690b
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-16 19:11:39 +0200

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,3 @@
-// debug
 pub static DEBUG                        : bool = true;
 pub static DEBUG_WITH_INPUT             : bool = false;
 pub static DEBUG_SHOW_CHAR              : bool = false;
@@ -7,5 +6,4 @@ pub static DEBUG_SHOW_TREE              : bool = true;
 pub static DEBUG_SHOW_SYMBOL_TABLE      : bool = false;
 pub static DEBUG_SHOW_ASSIGNMENT_TABLE  : bool = false;
 pub static DEBUG_SHOW_RESULT            : bool = true;
-// output
-pub static OUTPUT_VERBOSE               : bool = true;
+pub static OUTPUT_VERBOSE               : bool = true; // include comments in generated code
